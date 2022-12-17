06af3523a9777d99be6679519efd8e46e9957138
João Leal <jl@platforme.com>
2022-12-17 15:08:55 +0000

    feat: solution part 2

--- solutions/day-01.py
+++ solutions/day-01.py
@@ -1,15 +1,9 @@
 f = open("../inputs/day-01.txt")
 input = f.read()
 
-max = 0
-current_total = 0
-
-for line in input.split("\n"):
-    if not line:
-        max = max if max > current_total else current_total
-        current_total = 0
-        continue
-
-    current_total += int(line)
-
-print(max)
+elves = input.split("\n\n")
+food = [food.split("\n") for food in elves]
+totals = [sum([int(value) for value in food.split("\n")]) for food in elves]
+totals.sort(reverse=True)
+print("Part 1: %s" % totals[0])
+print("Part 2: %s" % sum(totals[:3]))
