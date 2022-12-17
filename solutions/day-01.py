89ca8f43c5eaa5f55890f33427bb3bac2641b857
João Leal <jl@platforme.com>
2022-12-17 15:10:36 +0000

    feat: shorter solution

--- solutions/day-01.py
+++ solutions/day-01.py
@@ -1,9 +1,7 @@
 f = open("../inputs/day-01.txt")
 input = f.read()
 
-elves = input.split("\n\n")
-food = [food.split("\n") for food in elves]
-totals = [sum([int(value) for value in food.split("\n")]) for food in elves]
+totals = [sum([int(value) for value in food.split("\n")]) for food in input.split("\n\n")]
 totals.sort(reverse=True)
 print("Part 1: %s" % totals[0])
 print("Part 2: %s" % sum(totals[:3]))
